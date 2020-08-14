b82608008d40b3757bf3d84411d71e9166090273
Juan <jgonzalez@vlocity.com>
2020-08-14 14:16:51 -0300

    Update Script to get latest Package Version

--- Get_Latest_Version_Package.sh
+++ Get_Latest_Version_Package.sh
@@ -6,15 +6,27 @@
 #---------------------------------------------
 
 DEVHUB_URL=$1
+PACKAGE_NAME=$2
 
+
+NUM_PACKAGES=$(jq -c '.packageDirectories | length' sfdx-project.json)
 echo "## Getting Version From Project File sfdx-project.json"
-VERSION=$(jq -r .packageDirectories[0].versionNumber sfdx-project.json)
-MAYOR_VERSION="$(cut -d'.' -f1 <<< "$VERSION")"
-MINOR_VERSION="$(cut -d'.' -f2 <<< "$VERSION")"
-PATCH_VERSION="$(cut -d'.' -f3 <<< "$VERSION")"
+for (( i=0; i<$NUM_PACKAGES; i++ ))
+do
+	PACKAGE=$(jq -r ".packageDirectories[${i}].package" sfdx-project.json)
+	echo "$PACKAGE - $PACKAGE_NAME"
+	if [ "$PACKAGE" == "$PACKAGE_NAME" ]; then
+		VERSION=$(jq -r .packageDirectories[0].versionNumber sfdx-project.json)
+		MAYOR_VERSION="$(cut -d'.' -f1 <<< "$VERSION")"
+		MINOR_VERSION="$(cut -d'.' -f2 <<< "$VERSION")"
+		PATCH_VERSION="$(cut -d'.' -f3 <<< "$VERSION")"
+	fi	
+done
 
-echo "## Getting Latest Build Number"
-RESULT=$(sfdx force:data:soql:query --json --usetoolingapi --targetusername="$DEVHUB_URL" --query="SELECT BuildNumber, SubscriberPackageVersionId FROM Package2Version WHERE MajorVersion=${MAYOR_VERSION} AND MinorVersion=${MINOR_VERSION} AND PatchVersion=${PATCH_VERSION} ORDER BY BuildNumber DESC LIMIT 1" )
+PACKAGE2_ID=$(jq -r ".packageAliases.${PACKAGE_NAME}"  sfdx-project.json)
+
+echo "## Getting Latest Build Number for Package: $PACKAGE_NAME ($PACKAGE2_ID) "
+RESULT=$(sfdx force:data:soql:query --json --usetoolingapi --targetusername="$DEVHUB_URL" --query="SELECT BuildNumber, SubscriberPackageVersionId FROM Package2Version WHERE MajorVersion=${MAYOR_VERSION} AND MinorVersion=${MINOR_VERSION} AND PatchVersion=${PATCH_VERSION} AND Package2Id='${PACKAGE2_ID}' ORDER BY BuildNumber DESC LIMIT 1" )
 BUILD_NUMBER=$(jq -r .result.records[0].BuildNumber <<< "$RESULT")
 PACKAGE_ID=$(jq -r .result.records[0].SubscriberPackageVersionId <<< "$RESULT")
 
